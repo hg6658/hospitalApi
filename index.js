36f9840b5dce82844253cada8557bb842b2b9664
hg6658 <hg6658@bennett.edu.in>
2023-04-04 14:18:00 +0530

    applied scaling

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const http_1 = require("http");
 const body_parser_1 = __importDefault(require("body-parser"));
 const websocket_1 = __importDefault(require("./config/websocket"));
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const node_cluster_1 = __importDefault(require("node:cluster"));
+const os_1 = require("os");
+const node_process_1 = __importDefault(require("node:process"));
+const numCPUs = (0, os_1.cpus)().length;
 const limiter = (0, express_rate_limit_1.default)({
     windowMs: 1000,
     max: 2,
@@ -25,14 +29,27 @@ var ss = passport_jwt_1.default;
 var dbs = mongoose_1.default;
 var ps = passport_1.default;
 const app = (0, express_1.default)();
-const httpServer = (0, http_1.createServer)(app);
-websocket_1.default.chatSocketInstance(httpServer);
-app.use(limiter);
-app.use(body_parser_1.default.json()); // support json encoded bodies
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use('/', require('./routes'));
-const server = httpServer.listen(8000, function () {
-    console.log(`Server is running on port: 8000`);
-});
-server.on('error', e => console.error("Error in the running server", e));
+if (node_cluster_1.default.isPrimary) {
+    console.log(`Primary ${node_process_1.default.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        node_cluster_1.default.fork();
+    }
+    node_cluster_1.default.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+}
+else {
+    const httpServer = (0, http_1.createServer)(app);
+    websocket_1.default.chatSocketInstance(httpServer);
+    app.use(limiter);
+    app.use(body_parser_1.default.json()); // support json encoded bodies
+    app.use(body_parser_1.default.urlencoded({ extended: true }));
+    app.use('/', require('./routes'));
+    const server = httpServer.listen(8000, function () {
+        console.log(`Server is running on port: 8000`);
+    });
+    server.on('error', e => console.error("Error in the running server", e));
+    console.log(`Worker ${node_process_1.default.pid} started`);
+}
 module.exports = app;
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ import { createServer,Server } from "http";
 import bodyParser from 'body-parser';
 import chatSocketInstance from './config/websocket';
 import rateLimit from 'express-rate-limit'
+import cluster from 'node:cluster';
+import { cpus } from 'os';
+import process from 'node:process';
+
+const numCPUs = cpus().length;
+
 
 const limiter = rateLimit({
 	windowMs: 1000, // 15 minutes
@@ -24,21 +30,36 @@ var ss = passportJwt;
 var dbs = db;
 var ps = passport;
 const app: Application = express();
-const httpServer: Server = createServer(app);
+if (cluster.isPrimary) {
+	console.log(`Primary ${process.pid} is running`);
+  
+	// Fork workers.
+	for (let i = 0; i < numCPUs; i++) {
+	  cluster.fork();
+	}
+  
+	cluster.on('exit', (worker, code, signal) => {
+	  console.log(`worker ${worker.process.pid} died`);
+	});
+  } else {
+    const httpServer: Server = createServer(app);
     
-chatSocketInstance.chatSocketInstance(httpServer);
-app.use(limiter);
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+    chatSocketInstance.chatSocketInstance(httpServer);
+    app.use(limiter);
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true }));
     
     
-app.use('/',require('./routes'));
+    app.use('/',require('./routes'));
     
     
-const server:Server = httpServer.listen(8000,function(){
-    console.log(`Server is running on port: 8000`);
-})
+    const server:Server = httpServer.listen(8000,function(){
+        console.log(`Server is running on port: 8000`);
+    })
 
-server.on('error', e => console.error("Error in the running server", e));
+    server.on('error', e => console.error("Error in the running server", e));
+
+    console.log(`Worker ${process.pid} started`);  
     
-export = app;
+  }
+  export=app;	  
