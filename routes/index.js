11f9e21ba492a22f5c9ec388599dda06d3cd15bd
hg6658 <hg6658@bennett.edu.in>
2023-04-04 20:11:15 +0530

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 const express_1 = __importDefault(require("express"));
 let router = express_1.default.Router();
+router.get('/', (req, res) => { res.send("<h1>Hello there please use postman to test this api.</h1>"); });
 router.use('/api', require('./api'));
 module.exports = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 let router = express.Router();
+router.get('/',(req,res)=>{res.send("<h1>Hello there please use postman to test this api.</h1>")});
 router.use('/api',require('./api'));
 
 export = router;
